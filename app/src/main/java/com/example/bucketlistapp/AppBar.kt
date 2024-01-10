ec09729bf8c2526dbc181e6ccbc5466baea99cb9
jaimenguyen168 <77992599+jaimenguyen168@users.noreply.github.com>
2024-01-10 14:57:29 -0500

    Give the Appbar a Back Icon that only appears when not on the HomeScreen

--- app/src/main/java/com/example/bucketlistapp/AppBar.kt
+++ app/src/main/java/com/example/bucketlistapp/AppBar.kt
@@ -2,11 +2,15 @@ package com.example.bucketlistapp
 
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.unit.dp
 
@@ -15,6 +19,21 @@ fun AppBar(
     title: String,
     onBackNavClicked: () -> Unit = {} // Do nothing by default
 ) {
+    val navigationIcon : (@Composable () -> Unit)? =
+        if (!title.contains("Bucket List")) {
+            {
+                IconButton(
+                    onClick = { onBackNavClicked() }
+                ) {
+                    Icon(
+                        imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                        contentDescription = "Arrow Back",
+                        tint = Color.White
+                    )
+                }
+            }
+        } else { null }
+
     TopAppBar(
         title = { Text(
                 text = title,
@@ -26,6 +45,6 @@ fun AppBar(
         },
         elevation = 3.dp,
         backgroundColor = colorResource(id = R.color.app_bar_color),
-//        navigationIcon = Icons.Default.Arrow
+        navigationIcon = navigationIcon
     )
 }
--- app/src/main/java/com/example/bucketlistapp/HomeScreen.kt
+++ app/src/main/java/com/example/bucketlistapp/HomeScreen.kt
@@ -1,16 +1,24 @@
 package com.example.bucketlistapp
 
+import android.widget.Toast
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 
 @Composable
 fun HomeScreen() {
+    val context = LocalContext.current
+
     Scaffold(
-        topBar = { AppBar(title = "Bucket List") }
+        topBar = {
+            AppBar(title = "Bucket List") {
+                Toast.makeText(context, "Button Clicked", Toast.LENGTH_SHORT).show()
+            }
+        }
     ) {
         LazyColumn(modifier = Modifier.fillMaxSize().padding(it)) {
 
