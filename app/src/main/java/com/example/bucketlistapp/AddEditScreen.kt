6030148363128871f331dff2ff7149e344fafa47
jaimenguyen168 <77992599+jaimenguyen168@users.noreply.github.com>
2024-01-10 21:59:03 -0500

    Prepare a basic ViewModel to take care of the changes of title and description

--- app/src/main/java/com/example/bucketlistapp/AddEditScreen.kt
+++ app/src/main/java/com/example/bucketlistapp/AddEditScreen.kt
@@ -6,12 +6,16 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material.Text
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 
 @Composable
@@ -20,12 +24,13 @@ fun AddEditScreen(
     viewModel: BucketListViewModel,
     navController: NavController
 ) {
+
+    val action = if (id != 0L) stringResource(id = R.string.update_bucket) else stringResource(id = R.string.add_bucket)
+
     Scaffold (
         topBar = { 
             CustomAppBar(
-                title = 
-                if (id != 0L) stringResource(id = R.string.update_bucket) 
-                else stringResource(id = R.string.add_bucket)
+                title = action
             )
         }
     ) {
@@ -39,6 +44,43 @@ fun AddEditScreen(
             Spacer(
                 modifier = Modifier.height(10.dp)
             )
+
+            AddEditView(
+                label = "Title",
+                value = viewModel.bucketListTitleState,
+                onValueChanged = {
+                    viewModel.onTitleChanged(it)
+                }
+            )
+
+            AddEditView(
+                label = "Description",
+                value = viewModel.bucketListDescState,
+                onValueChanged = {
+                    viewModel.onDescriptionChanged(it)
+                }
+            )
+
+            Spacer(
+                modifier = Modifier.height(10.dp)
+            )
+
+            Button(onClick = {
+                if (viewModel.bucketListTitleState.isNotEmpty()
+                    && viewModel.bucketListDescState.isNotEmpty()) {
+                    // TODO Update
+                } else {
+                    // TODO Add
+                }
+            }) {
+                Text(
+                    text = action,
+                    style = TextStyle(
+                        fontSize = 18.sp
+                    )
+                )
+            }
         }
     }
 }
+
--- app/src/main/java/com/example/bucketlistapp/BucketListViewModel.kt
+++ app/src/main/java/com/example/bucketlistapp/BucketListViewModel.kt
@@ -1,7 +1,19 @@
 package com.example.bucketlistapp
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 
 class BucketListViewModel : ViewModel() {
+    var bucketListTitleState by mutableStateOf("")
+    var bucketListDescState by mutableStateOf("")
 
+    fun onTitleChanged(newTitle: String) {
+        bucketListTitleState = newTitle
+    }
+
+    fun onDescriptionChanged(newDescription: String) {
+        bucketListDescState = newDescription
+    }
 }
