8151347f4ed99e067c000ba2735cc3e912a7292f
jaimenguyen168 <77992599+jaimenguyen168@users.noreply.github.com>
2024-01-10 23:49:58 -0500

    Setup the Repository

---
+++ app/src/main/java/com/example/bucketlistapp/data/BucketListRepository.kt
@@ -0,0 +1,22 @@
+package com.example.bucketlistapp.data
+
+import kotlinx.coroutines.flow.Flow
+
+class BucketListRepository(private val bucketListDAO: BucketListDAO) {
+
+    suspend fun addAnItem(item: BucketLister) {
+        bucketListDAO.addAnItem(item)
+    }
+
+    suspend fun updateAnItem(item: BucketLister) {
+        bucketListDAO.updateAnItem(item)
+    }
+
+    suspend fun deleteAnItem(item: BucketLister) {
+        bucketListDAO.deleteAnItem(item)
+    }
+
+    fun getAnItem(id: Long) : Flow<BucketLister> = bucketListDAO.getAnItemById(id)
+
+    fun getAllItems() : Flow<List<BucketLister>> = bucketListDAO.getAllItems()
+}
